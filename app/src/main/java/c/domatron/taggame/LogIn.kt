1205d56e7115c03a9d104483c34e2b63799c06b1
Dominic Triano <d.triano0100@gmail.com>
2019-03-21 13:55:20 -0400

    Changed Color Scheme
    More colors added, much more visibility on the navigation bar and for the background once login gets implimented. Commented out and took a few lines out of tagRef since it was giving a threading error.

--- app/src/main/java/c/domatron/taggame/LogIn.kt
+++ app/src/main/java/c/domatron/taggame/LogIn.kt
@@ -5,18 +5,14 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 
 import kotlinx.android.synthetic.main.activity_log_in.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class LogIn : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
 }
--- app/src/main/java/c/domatron/taggame/TagRef.kt
+++ app/src/main/java/c/domatron/taggame/TagRef.kt
@@ -3,16 +3,16 @@ package c.domatron.taggame
 import android.provider.DocumentsContract
 
 class TagRef {
-
-    fun TagRef(User: String)
-    {
-        var Status = false
-        var TID = 0
-    }
-
-    fun readXml(): DocumentsContract.Document {
-        val xmlFile = java.net.URL("web.cs.sunypoly.edu/~trianod/TagGame.xml")
-
-
-    }
+//
+//    fun TagRef(User: String)
+//    {
+//        var Status = false
+//        var TID = 0
+//    }
+//
+//    fun readXml(): DocumentsContract.Document {
+//        val xmlFile = java.net.URL("web.cs.sunypoly.edu/~trianod/TagGame.xml")
+//
+//
+//    }
 }
